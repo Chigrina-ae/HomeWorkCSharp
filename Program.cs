fb67048585cef577561ecf163e0e67b7162e2fe5
Anna Chigrina <aechigrina@mail.ru>
2022-03-03 00:18:27 +0300

    add active program

--- Program.cs
+++ Program.cs
@@ -3,85 +3,85 @@
 
 // Задача 2. Напишите программу, которая на вход принимает два числа и выдает, какое число большее,а какое меньшее
 
-// Console.WriteLine ("Введите первое число");
+ Console.WriteLine ("Введите первое число");
 
-// int ferst = Convert.ToInt32 (Console.ReadLine()); // число 1. Конвертируем текст в число
+int ferst = Convert.ToInt32 (Console.ReadLine()); // число 1. Конвертируем текст в число
 
-// Console.WriteLine ("Введите второе число");
+Console.WriteLine ("Введите второе число");
 
-// int second = Convert.ToInt32 (Console.ReadLine());  // число 2. Конвертируем текст в число
+int second = Convert.ToInt32 (Console.ReadLine());  // число 2. Конвертируем текст в число
 
-// if (ferst==second) 
-//      {Console.WriteLine ($"Числа равны = {ferst}");
-//      }
+if (ferst==second) 
+     {Console.WriteLine ($"Числа равны = {ferst}");
+     }
 
-// else if (ferst>second)
-//      {Console.Write ($"Наибольшее из чисел - первое = {ferst}");
-//      }
+else if (ferst>second)
+     {Console.Write ($"Наибольшее из чисел - первое = {ferst}");
+     }
 
-// else
-//  {Console.Write ($"Наибольшее из чисел - второе = {second}");}
+else
+ {Console.Write ($"Наибольшее из чисел - второе = {second}");}
 
 
 // Задача 4_(решение 1). Напишите программу, которая принимает на вход три числа и выдаетмаксимальное из этих чисел
 
-// Console.WriteLine ("Введите первое число");
+Console.WriteLine ("Введите первое число");
 
-// int ferst = Convert.ToInt32 (Console.ReadLine()); // число 1. Конвертируем текст в число
+int ferst = Convert.ToInt32 (Console.ReadLine()); // число 1. Конвертируем текст в число
 
-// Console.WriteLine ("Введите второе число");
+Console.WriteLine ("Введите второе число");
 
-// int second = Convert.ToInt32 (Console.ReadLine());  // число 2. Конвертируем текст в число
+int second = Convert.ToInt32 (Console.ReadLine());  // число 2. Конвертируем текст в число
 
-// Console.WriteLine ("Введите третье число");
+Console.WriteLine ("Введите третье число");
 
-// int third = Convert.ToInt32 (Console.ReadLine());  // число 3. Конвертируем текст в число
+int third = Convert.ToInt32 (Console.ReadLine());  // число 3. Конвертируем текст в число
 
-// int max = 0;
+int max = 0;
 
 
-// if (max< ferst)
-//   {max = ferst;
-//   }
+if (max< ferst)
+  {max = ferst;
+  }
 
-// if (max< second)
-//   {max = second;
-//   }
+if (max< second)
+  {max = second;
+  }
   
-// if (max< third)
-//   {max = third;
-//   }
+if (max< third)
+  {max = third;
+  }
 
-// Console.WriteLine ($"Наибольшее число из трех = {max}");
+Console.WriteLine ($"Наибольшее число из трех = {max}");
 
 
 //  // Задача 4_(решение 2). Напишите программу, которая принимает на вход три числа и выдаетмаксимальное из этих чисел
 
-// Console.WriteLine ("Введите 3 числа для сравнения");
+Console.WriteLine ("Введите 3 числа для сравнения");
  
-//  int [] numbers = new int [3];
+ int [] numbers = new int [3];
  
-//  //заполняем массив
+ //заполняем массив
 
-// numbers [0] = Convert.ToInt32 (Console.ReadLine());
-// numbers [1] = Convert.ToInt32 (Console.ReadLine());
-// numbers [2] = Convert.ToInt32 (Console.ReadLine());
+numbers [0] = Convert.ToInt32 (Console.ReadLine());
+numbers [1] = Convert.ToInt32 (Console.ReadLine());
+numbers [2] = Convert.ToInt32 (Console.ReadLine());
 
-// int i = 0;
-// int max = numbers[0];
+int i = 0;
+int max = numbers[0];
 
-// while (i<3) 
-//   {if (max<numbers[i])
-//          {max =numbers[i];
+while (i<3) 
+  {if (max<numbers[i])
+         {max =numbers[i];
          
-//          }
+         }
      
          
-//   i=i+1;   
-//   }
+  i=i+1;   
+  }
 
 
-// Console.WriteLine ($"Наибольшее число из трех = {max}");
+Console.WriteLine ($"Наибольшее число из трех = {max}");
 
 
 
